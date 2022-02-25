5fb986f1ed45470bbbbdbde5ea35d9ec7923da7f
Alison Harman <alison_harman@sympatico.ca>
2022-02-25 13:04:02 -0500

    apply conditional styling to component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,15 @@
 import React from "react";
+import classNames from '../../node_modules/classnames';
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+
+  let dayClass = classNames('day-list__item ', { 'day-list__item--selected': props.selected }, {'day-list__item--full': props.spots===0});
+
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
   );
 }
