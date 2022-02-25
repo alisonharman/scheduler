073062c73d49bdd3dc3bac34a1bdb4eb81a82b01
Alison Harman <alison_harman@sympatico.ca>
2022-02-25 12:15:17 -0500

    refactor conditional classes on button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,12 @@
 import React from "react";
+import classNames from "../../node_modules/classnames";
+//import classNames from classnames;  // does not work.... why?
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   let buttonClass ="button";
-
-   if (props.confirm) {
-      buttonClass += " button--confirm";
-   }
-   if (props.danger) {
-      buttonClass += " button--danger";
-   }
+   
+   let buttonClass = classNames('button ', { 'button--confirm': props.confirm }, {'button--danger': props.danger});
 
    return <button onClick={props.onClick} disabled={props.disabled} className={buttonClass}>{props.children}</button>;
 }
