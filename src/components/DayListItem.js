24b6d573eb38d51476be0472ccfa35de4a4e181d
Alison Harman <alison_harman@sympatico.ca>
2022-02-25 15:05:10 -0500

    add semi-colon

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,10 +6,22 @@ export default function DayListItem(props) {
 
   let dayClass = classNames('day-list__item ', { 'day-list__item--selected': props.selected }, {'day-list__item--full': props.spots===0});
 
+  function formatSpots(spots) {
+   if (!spots) {
+     return "no spots remaining"
+   } 
+   if (spots === 1) {
+     return "1 spot remaining"
+   }
+   else {
+     return `${spots} spots remaining`
+   }
+  };
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)} </h3>
     </li>
   );
 }
