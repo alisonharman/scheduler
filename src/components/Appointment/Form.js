9987ae40e0ccd0d839743ed9aba225a62fe2a267
Alison Harman <alison_harman@sympatico.ca>
2022-03-01 12:57:53 -0500

    add state for interviewer and student in Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,39 +1,46 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./styles.scss"
 import Button from "../Button"
 import InterviewerList from "../InterviewList"
 
 export default function Form(props) {
 
-  const [word, setWord] = useState("");
+  const [student, setStudent] = useState(props.student || "")
+  const [interviewer, setInterviewer] = useState(props.interviewer || null)
+
+  function reset() {
+    setStudent("")
+    setInterviewer(null)
+  };
+
+  function cancel() {
+    reset();
+    props.onCancel();
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
-            name={props.name}
+            name="name"
             type="text"
             placeholder="Enter Student Name"
-            value={word}
-            onChange={(event) => setWord(event.target.value)}
-          /*
-            This must be a controlled component
-            your code goes here
-          */
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
           />
         </form>
         <InterviewerList
           interviewers={props.interviewers}
-          interviewer={props.interviewer}
-        /* your code goes here */
+          onChange={setInterviewer}
+          value={interviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel} >Cancel</Button>
-          <Button confirm  onClick={props.onSave}>Save</Button>
+          <Button danger onClick={cancel} >Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
     </main>
