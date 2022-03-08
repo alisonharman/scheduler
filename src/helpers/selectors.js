794068c4e4effd8735595e4f56c52c9b7aac5068
Alison Harman <alison_harman@sympatico.ca>
2022-03-08 12:07:27 -0500

    create and test selector for returning appointments of a given day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,30 @@
+export function getAppointmentsForDay(state, day) {
+  //... returns an array of appointments for that day
+  // first, need to relate day like "Monday" to id "1", for example
+  const findAppointmentsBasedOnDay = (day) => {
+    for (const element of state.days) {
+      if (element.name === day) {
+        return element.appointments
+      }
+    }
+  }
+  // assign to local variable
+  const appointmentArray = findAppointmentsBasedOnDay(day);
+  // appointments array will be the returned value 
+  const appointments = [];
+  // if no appointments in appointmentArray
+  if (!appointmentArray) {
+    return appointments;
+  }
+// iterate through appointments and push each object into a new array
+  for (const appointment of appointmentArray) {
+    const id = appointment
+    for (const property in state.appointments) {
+        if (property == id) {
+          appointments.push(state.appointments[property])
+        }
+    }
+  }
+  return appointments;
+
+}
