2dc7ad165bada48525ed129c64e5bf84b5341285
Alison Harman <alison_harman@sympatico.ca>
2022-03-09 17:09:55 -0500

    request at api/interviewers to display name of interviewer

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,14 +4,15 @@ import axios from "axios"
 import "components/Application.scss";
 import DayList from "./DayList"
 import Appointment from "./Appointment"
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
 
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   })
 
   let dailyAppointments = [];
@@ -21,18 +22,24 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       Promise.resolve(axios.get("api/days")),
-      Promise.resolve(axios.get("api/appointments"))
+      Promise.resolve(axios.get("api/appointments")),
+      Promise.resolve(axios.get("api/interviewers"))
     ]).then((all) => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }))
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }))
     })
   }, [])
 
   dailyAppointments = getAppointmentsForDay(state, state.day);
-
   // turn appointments object into array
   const allAppointments = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview)
     return (
-      <Appointment key={appointment.id} {...appointment} />
+      <Appointment 
+      key={appointment.id}
+      id = {appointment.id}
+      time = {appointment.time}
+      interview = {interview}
+       />
     )
   })
 
