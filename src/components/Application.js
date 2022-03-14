ddfe2ea4a56c2f4cd66699a62bcbb77ddfe79361
Alison Harman <alison_harman@sympatico.ca>
2022-03-14 14:58:33 -0400

    refactor code to use custom hook useApplicationData for actions to change state

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,94 +5,21 @@ import "components/Application.scss";
 import DayList from "./DayList"
 import Appointment from "./Appointment"
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
+import useApplicationData from "hooks/useApplicationData"
 
 export default function Application(props) {
 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  })
-
-  let dailyAppointments = [];
-  let dailyInterviewers = [];
-
-  const setDay = day => setState({ ...state, day });
-
-  useEffect(() => {
-    Promise.all([
-      Promise.resolve(axios.get("api/days")),
-      Promise.resolve(axios.get("api/appointments")),
-      Promise.resolve(axios.get("api/interviewers"))
-    ]).then((all) => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }))
-    })
-  }, [])
-
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    }
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    const request = axios.put(`http://localhost:8001/api/appointments/${id}`, {interview: interview})
-
-    return request
-    .then(result => {setState({
-      ...state,
-      appointments
-     }) });
-
-     // the below leaves an uncatched error?
-    return Promise.all([
-      Promise.resolve(axios.put(`http://localhost:8001/api/appointments/${id}`, {interview: interview})),
-      Promise.resolve( setState({
-        ...state,
-        appointments
-       }))
-    ])
-
-  };
-
-  function cancelInterview(id) {
-    // need to set interview object to null based on appointment ID
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    }
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-/*
-     return Promise.all([
-      Promise.resolve(axios.delete(`http://localhost:8001/api/appointments/${id}`, {interview: null})),
-      Promise.resolve( setState({
-        ...state,
-        appointments
-       }))
-    ])
-    */
-
-    const request = axios.delete(`http://localhost:8001/api/appointments/${id}`, {interview: null})
-
-    return request
-    .then(result => {setState({
-      ...state,
-      appointments
-     }) });
-
-  };
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
   // transform appointment data into an array 
-  dailyAppointments = getAppointmentsForDay(state, state.day);
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
   // transform interviewers data into an array
-  dailyInterviewers = getInterviewersForDay(state, state.day)
+  const dailyInterviewers = getInterviewersForDay(state, state.day)
   // turn appointments object into array of appointment objects
   const allAppointments = dailyAppointments.map(appointment => {
     const interview = getInterview(state, appointment.interview)
