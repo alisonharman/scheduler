ac3e5a1e2102fc4118945a593db668a7b4f951b1
Alison Harman <alison_harman@sympatico.ca>
2022-03-15 23:23:31 -0400

    fix bug in updateSpots function

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -31,27 +31,17 @@ export default function useVisualMode(initial) {
       ...state.appointments,
       [id]: appointment
     };
-    const days = updateSpots(true)
+    const days = updateSpots(appointments)
 
     const request = axios.put(`http://localhost:8001/api/appointments/${id}`, { interview: interview })
 
     return request
       .then(() => {
-        setState(prev => ({
-          ...prev,
+        setState({
+          ...state,
           appointments, days
-        }))
+        })
       });
-/*
-    // the below leaves an uncatched error?
-    return Promise.all([
-      Promise.resolve(axios.put(`http://localhost:8001/api/appointments/${id}`, { interview: interview })),
-      Promise.resolve(setState({
-        ...state,
-        appointments
-      }))
-    ])
-*/
   };
 
   function cancelInterview(id) {
@@ -64,7 +54,7 @@ export default function useVisualMode(initial) {
       ...state.appointments,
       [id]: appointment
     };
-    const days = updateSpots()
+    const days = updateSpots(appointments)
    
 
     const request = axios.delete(`http://localhost:8001/api/appointments/${id}`, { interview: null })
@@ -78,25 +68,30 @@ export default function useVisualMode(initial) {
       });
   };
 
-  function updateSpots(interviewAdded=false) {
-    // need to find the index of days array whose "spots" needs to be updated
-    // have day of Week (e.g. "Monday") from state
+  function updateSpots(appointments) {
+    // based on state.day (e.g. "Monday"), find the object representing this day in the state.days array
+    // extract day id and appointments (array of ids) of this object
     let dayIndex = 0;
+    let appointmentIds = [];
     for (const day of state.days) {
       if (state.day === day.name) {
         dayIndex = day.id-1
+        appointmentIds = day.appointments
       }
     }
     // deep copy of day object
     const day = {...state.days[dayIndex]}
+
+    // need to extract the total number of null interview based on appointment ids
+    let nullSpots = 0;
+    for (const id of appointmentIds) {
+      const obj = appointments[id]
+      if (obj.interview === null) {
+        nullSpots++
+      }
+    }
     
-    // if adding an interview like in bookInterview, then number of spots decreases by one
-    if (interviewAdded) {
-      day.spots -= 1
-    }
-    else {
-      day.spots += 1
-    }
+    day.spots = nullSpots;
 
     // build a new days array of state object by replacing the day with correct number of spots
     let days = [...state.days];
