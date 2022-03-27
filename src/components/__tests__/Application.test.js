c91f2fcc9e12856f078f0e29dfdf5c7b8bd5ee7b
Alison Harman <alison_harman@sympatico.ca>
2022-03-27 11:55:38 -0400

    add first Application integration test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,44 +1,16 @@
 import React from "react";
 import Application from "components/Application";
 import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
-import '@testing-library/jest-dom';
-/*
-jest.unmock('axios')
-import axios from "axios"
-const MockAdapter = require("axios-mock-adapter");
-import {fixtures} from "../../__mocks__/axios"
+//import '@testing-library/jest-dom';
+import axios from 'axios'
+//jest.mock('../../__mocks__/axios')
 
-beforeAll(() => {
-  mock.reset();
-});
+it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+  const { getByText } = render(<Application />);
 
-// This sets the mock adapter on the default instance
-let mock = new MockAdapter(axios);
-
-mock.onGet("/api/days").reply(200, {
-  data: fixtures.days
-});
-
-mock.onGet("/api/appointments").reply(200, {
-  data: fixtures.appointments
-});
-
-mock.onGet("/api/interviewers").reply(200, {
-  data: fixtures.interviewers
-});
-*/
-
-afterEach(cleanup);
-
-describe("Form", () => {
-  xit("defaults to Monday and changes the schedule when a new day is selected", async () => {
-
-    const { getByText } = render(<Application />);
-
-     return waitForElement(() => getByText("Monday")).then(() => {
-      fireEvent.click(getByText("Tuesday"));
-      expect(getByText("Leopold Silvers")).toBeInTheDocument();
-    });
+  return waitForElement(() => getByText("Monday"))
+  .then(() => {
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
-
 });
