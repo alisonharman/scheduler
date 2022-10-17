94a4767c3c804daa06f76a4f4ce55df63210acda
Alison Harman <alison_harman@sympatico.ca>
2022-10-17 17:28:50 -0400

    start test for Application component

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,16 +1,35 @@
 import React from "react";
 import Application from "components/Application";
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import {
+  render,
+  cleanup,
+  waitForElement,
+  fireEvent,
+  getByText,
+} from "@testing-library/react";
 //import '@testing-library/jest-dom';
-import axios from 'axios'
+import axios from "axios";
 //jest.mock('../../__mocks__/axios')
 
-it("defaults to Monday and changes the schedule when a new day is selected", async () => {
-  const { getByText } = render(<Application />);
+afterEach(cleanup);
 
-  return waitForElement(() => getByText("Monday"))
-  .then(() => {
+describe("Application", () => {
+
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+
+    await waitForElement(() => getByText("Monday"));
+
     fireEvent.click(getByText("Tuesday"));
+
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
+
+  it("render the Application without crashing, loads data", async () => {
+    const { container } = render(<Application/>);
+
+    // wait until the text "Archie Cohen" is displayed
+    await waitForElement(() => getByText(container, "Archie Cohen"))
+
+  })
 });
