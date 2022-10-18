d633c1861c0cf7f3cf9c7777d2db8c94c15760f6
Alison Harman <alison_harman@sympatico.ca>
2022-10-18 17:17:49 -0400

    add new actions of adding student name, click save button for testing

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -7,8 +7,11 @@ import {
   fireEvent,
   getByText,
   getAllByTestId,
-  getByAltText
+  getByAltText,
+  getByPlaceholderText,
 } from "@testing-library/react";
+import "../../__mocks__/axios";
+
 //import '@testing-library/jest-dom';
 import axios from "axios";
 //jest.mock('../../__mocks__/axios')
@@ -16,7 +19,6 @@ import axios from "axios";
 afterEach(cleanup);
 
 describe("Application", () => {
-
   it("defaults to Monday and changes the schedule when a new day is selected", async () => {
     const { getByText } = render(<Application />);
 
@@ -28,10 +30,10 @@ describe("Application", () => {
   });
 
   it("render the Application without crashing, loads data, books an interview", async () => {
-    const { container } = render(<Application/>);
+    const { container } = render(<Application />);
 
-    // wait until the text "Archie Cohen" is displayed
-    await waitForElement(() => getByText(container, "Archie Cohen"))
+    // 2. wait until the text "Archie Cohen" is displayed
+    await waitForElement(() => getByText(container, "Archie Cohen"));
 
     // 3. click the "Add" button on the first empty appointment
     const appointments = getAllByTestId(container, "appointment");
@@ -39,5 +41,17 @@ describe("Application", () => {
     const addButton = getByAltText(appointment, "Add");
     fireEvent.click(addButton);
 
-  })
+    // 4. Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
+    const inputField = getByPlaceholderText(appointment, /enter student name/i);
+    fireEvent.change(inputField, {
+      target: { value: "Lydia Miller-Jones" },
+    });
+
+    // 5. Click the first interviewer in the list.
+    const interviewer = getByAltText(appointment, "Sylvia Palmer");
+    fireEvent.click(interviewer);
+
+    // 6. Click the "Save" button on that same appointment.
+   
+  });
 });
